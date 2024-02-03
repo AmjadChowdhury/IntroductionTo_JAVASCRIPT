250b407e141d3671afab4b4ada5e18fee8baad11
AmjadChowdhury <amjadchowdhury247@gmail.com>
2024-02-03 09:57:44 +0000

    function deep

--- function.js
+++ function.js
@@ -30,3 +30,41 @@ console.log("borgo function teke paisi : ",value);
 console.log("____________________________");
 value = borgo(121);
 console.log("borgo function teke paisi : ",value);
+
+// condition r upor depend kore return korbe...
+function isEven(numbers){
+    if(numbers % 2 === 0)
+    {
+        return true;
+    }
+    return false;
+}
+let isEvenKi = isEven(12);
+console.log(isEvenKi);
+
+isEvenKi = isEven(153);
+console.log(isEvenKi);
+
+
+// string function...
+function fullName(str,str2){
+    const name = str + str2;
+    console.log(name);
+}
+
+fullName('Amjad ','chy')
+
+
+function arraySum(array){
+    // console.log(array);
+    let sum = 0;
+    for(const num of array){
+        console.log(num);
+        sum +=num;
+    }
+    return sum;
+}
+
+let array = [10, 5,9,1,3,12,6];
+let tot = arraySum(array);
+console.log('total of array: ',tot);
