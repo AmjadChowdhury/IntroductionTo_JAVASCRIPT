94a6bebeb68ce08ad5c51c616c7e0677e20a3a24
AmjadChowdhury <amjadchowdhury247@gmail.com>
2024-02-04 07:05:37 +0000

    swap and math

---
+++ math.js
@@ -0,0 +1,20 @@
+let minA = Math.min(10,15,3,12,20);
+console.log(minA); // 3
+
+let maxA = Math.max(10,15,3,12,20);
+console.log(maxA); // 20
+
+let num = Math.round(2.67);
+console.log(num); // 3
+
+num = Math.ceil(2.67);
+console.log(num); // 3
+
+num = Math.floor(2.67);
+console.log(num); // 2
+
+num = Math.round(Math.random()*10);
+console.log(num);
+
+num = Math.sqrt(16);
+console.log(num);
---
+++ problem/swap.js
@@ -0,0 +1,13 @@
+let a = 7;
+let b = 5;
+let tmp;
+
+console.log("Before Swapping",a,b);
+
+tmp = a;
+a = b;
+b = tmp;
+
+// [a,b] = [b,a];
+
+console.log("After Swap",a,b);
